6b69b80bcc91ef021c2b2b935af31627be90a338
benzics <benjaminnicholas29@gmail.com>
2022-12-07 12:00:33 +0100

    Product service added

---
+++ app/Services/ProductService.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Services;
+
+use App\Models\Product;
+
+/**
+*Automatically generated service
+*Author: Benjamin Ojobo
+*https://github.com/benzics
+*/
+
+
+class ProductService {
+	
+	/**
+	 * Gets products paginated
+	 * @param int $limit
+	 * @return
+	 */
+	public function get_products(int $limit = 15)
+	{
+		$products = Product::paginate($limit);
+
+		return $products;
+	}
+
+	/**
+	 * Gets all products 
+	 * @return
+	 */
+	public function all_products()
+	{
+		$products = Product::all();
+
+		return $products;
+	}
+
+	/**
+	 * Gets a specific product
+	 * @param int $product The product id
+	 * @return
+	 */
+	public function get_product(int $product)
+	{
+		$product = Product::find($product);
+
+		return $product;
+	}
+}
+
+
