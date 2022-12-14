3223127a5e1743151d5378abe12a17b17a487288
benzics <benjaminnicholas29@gmail.com>
2022-12-14 05:10:29 +0100

    Initial settings test

--- tests/Feature/AdminSettingsTest.php
+++ tests/Feature/AdminSettingsTest.php
@@ -5,18 +5,43 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Setting;
+use App\Models\User;
 
 class AdminSettingsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    private $_user;
+
+    private $_data = ['name' => 'Test setting', 'value' => 'Test'];
+
+    private function _factory()
+    {
+        return Setting::factory()->create();
     }
+
+    public function setUp(): void 
+    {
+        parent::setUp();
+        $this->seed();
+        $this->_user = User::find(1);
+    }
+
+    public function test_settings_page()
+    {
+        $this->_factory();
+        $response = $this->actingAs($this->_user)->get(route('admin.settings'));
+        $response->assertOk()->assertValid();
+    }
+
+    public function test_settings_edit()
+    {
+        $this->_factory();
+
+        $response = $this->actingAs($this->_user)->post(route('admin.settings'), $this->_data);
+
+        $response->assertValid()->assertSessionHas('notify')->assertRedirect(route('admin.settings'));
+    }
+
 }
