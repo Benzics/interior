4cabd00a41b8b809f3ddc54d81317cb7f7b07dfb
benzics <benjaminnicholas29@gmail.com>
2023-02-09 17:43:00 +0100

    refactored booking test in case our codebase grows

--- tests/Feature/BookingTest.php
+++ tests/Feature/BookingTest.php
@@ -5,9 +5,9 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
+$data = ['email' => 'test@site.com', 'product_id' => '1', 'name' => 'test'];
 
-it('books a product', function () {
-    $data = ['email' => 'test@site.com', 'product_id' => '1', 'name' => 'test'];
+it('books a product', function () use ($data) {
     $response = $this->postJson(route('api.book'), $data);
 
     $response->assertJson(['success' => 'true']);
